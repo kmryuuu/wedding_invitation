b82a7adc56759a7c8794477131f6ba3cebc29590
kmryuuu <138556024+kmryuuu@users.noreply.github.com>
2024-02-02 22:00:47 +0900

    카카오톡 공유하기 추가

--- src/components/contact/accountData.js
+++ src/components/contact/accountData.js
@@ -19,22 +19,22 @@ export const brideData = [
   {
     modifier: '아버지',
     name: '류중현',
-    bank: '국민은행 ',
-    account: '1234-567-890123',
-    payUrl: 'https://qr.kakaopay.com/FEi3Gomyq',
+    bank: '우리은행',
+    account: '1002-129-861789',
+    payUrl: 'https://qr.kakaopay.com/Ej9Btntk7',
   },
   {
     modifier: '어머니',
     name: '이효숙',
-    bank: '국민은행 ',
-    account: '1234-567-890123',
+    bank: '기업은행',
+    account: '0114465866',
     payUrl: 'https://qr.kakaopay.com/FEi3Gomyq',
   },
   {
     modifier: '신부',
     name: '류경민',
     bank: '국민은행 ',
-    account: '1234-567-890123',
+    account: '511302-01-419060',
     payUrl: 'https://qr.kakaopay.com/FEi3Gomyq',
   },
 ];
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -5,13 +5,48 @@ import logoKaKao from '../assets/kakaotalk.png';
 import logoGithub from '../assets/github.png';
 
 export default function Footer() {
+  const kakaoButton = () => {
+    const kakaoImage = process.env.PUBLIC_URL + '/gallery/gallery-13.jpg';
+    const KAKAO_KEY = process.env.REACT_APP_KAKAO_KEY;
+
+    if (window.Kakao) {
+      const kakao = window.Kakao;
+
+      if (!kakao.isInitialized()) {
+        kakao.init(KAKAO_KEY);
+      }
+
+      kakao.Share.sendDefault({
+        objectType: 'feed',
+        content: {
+          title: '상성 & 경민 결혼합니다',
+          description: '2024. 04. 28 일요일 오후 12:00',
+          imageUrl: kakaoImage,
+          link: {
+            mobileWebUrl: 'http://localhost:3000',
+            webUrl: 'http://localhost:3000',
+          },
+        },
+        buttons: [
+          {
+            title: '청첩장 보러가기',
+            link: {
+              mobileWebUrl: 'http://localhost:3000',
+              webUrl: 'http://localhost:3000',
+            },
+          },
+        ],
+      });
+    }
+  };
+
   return (
     <>
       <ImgFooter src={imgFooter} />
       <FooterText className="bold">상성 & 경민</FooterText>
       <FooterText>예쁘게 잘 살겠습니다 ♡</FooterText>
       <FooterContainer>
-        <ShareButton>
+        <ShareButton onClick={kakaoButton}>
           <KakaoLogo src={logoKaKao} />
           카카오톡 공유하기
         </ShareButton>
