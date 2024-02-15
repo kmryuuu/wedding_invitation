4102381162e579c20f951e9cfbe1fd1a5f51c130
kmryuuu <138556024+kmryuuu@users.noreply.github.com>
2024-02-15 11:35:04 +0900

    필요 없는 폴더 삭제

--- src/hook/useKakaoLoader.jsx
+++
@@ -1,8 +0,0 @@
-// import { useKakaoLoader as useKakaoLoaderOrigin } from 'react-kakao-maps-sdk';
-
-// export default function useKakaoLoader() {
-//   useKakaoLoaderOrigin({
-//     appkey: process.env.REACT_APP_KAKAO_KEY,
-//     libraries: ['clusterer', 'drawing', 'services'],
-//   });
-// }
